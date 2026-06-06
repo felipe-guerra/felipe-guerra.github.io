3a5091b3104fa40125378a108b37b8a4cfe138ce
Felipe Guerra <felipe.david.guerra@gmail.com>
2026-06-06 16:21:08 -0500

    feat: migración a Next.js hola mundo en rama main

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Le dice a Next.js que compile a HTML, CSS y JS puros
+  basePath: '/felipe-guerra.github.io', // Configura la subruta correcta para tu repositorio
+  images: {
+    unoptimized: true, // Desactiva la optimización dinámica de imágenes en servidor (GitHub Pages no la soporta)
+  },
 };
 
 export default nextConfig;
